dc20c893c14fed5d3a0b902d0f1ac4eac5cb0418
Rosieeeeeeeeee <126637528+Rosieeeeeeeeee@users.noreply.github.com>
2023-03-15 11:58:26 +0800

    Add files via upload

---
+++ Greetings Generator/GreetingsGenerator.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.displayBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.firstNameInputField = new System.Windows.Forms.TextBox();
+            this.lastNameInputField = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // displayBtn
+            // 
+            this.displayBtn.Location = new System.Drawing.Point(220, 282);
+            this.displayBtn.Name = "displayBtn";
+            this.displayBtn.Size = new System.Drawing.Size(80, 50);
+            this.displayBtn.TabIndex = 0;
+            this.displayBtn.Text = "Submit";
+            this.displayBtn.UseVisualStyleBackColor = true;
+            this.displayBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(220, 348);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(80, 50);
+            this.exitBtn.TabIndex = 1;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // firstNameInputField
+            // 
+            this.firstNameInputField.Location = new System.Drawing.Point(172, 146);
+            this.firstNameInputField.Name = "firstNameInputField";
+            this.firstNameInputField.Size = new System.Drawing.Size(189, 20);
+            this.firstNameInputField.TabIndex = 2;
+            this.firstNameInputField.Text = "Enter your First Name";
+            this.firstNameInputField.TextChanged += new System.EventHandler(this.firstNameInputField_TextChanged);
+            // 
+            // lastNameInputField
+            // 
+            this.lastNameInputField.Location = new System.Drawing.Point(172, 211);
+            this.lastNameInputField.Name = "lastNameInputField";
+            this.lastNameInputField.Size = new System.Drawing.Size(189, 20);
+            this.lastNameInputField.TabIndex = 3;
+            this.lastNameInputField.Text = "Enter your Last Name";
+            this.lastNameInputField.TextChanged += new System.EventHandler(this.lastNameInputField_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(138, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(276, 34);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Greetings Generator";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(169, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "First Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(169, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Last Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 421);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lastNameInputField);
+            this.Controls.Add(this.firstNameInputField);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.displayBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button displayBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.TextBox firstNameInputField;
+        private System.Windows.Forms.TextBox lastNameInputField;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
